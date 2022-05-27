6d64b59a3d5ed41ad623a0662296869deb31cf5d
Your Name <you@example.com>
2022-05-27 09:41:43 +0300

    New

---
+++ src/index.js
@@ -0,0 +1,11 @@
+document.addEventListener('load', () => {
+
+    const submit = document.querySelectorAll('submit');
+    const output = document.querySelector('output');
+
+    submit.addEventListener('click', () => {
+        let a =
+    })
+
+
+})
